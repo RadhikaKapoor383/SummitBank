729d4d59dcc2551948fec8cabe34fdbbb8ad07e0
Radhika kapoor <radhikakapoor383@gmail.com>
2024-07-30 22:36:09 +0500

    Add files via upload

---
+++ BankSystem.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package banksystem;
+
+/**
+ *
+ * @author lalaAhsan
+ */
+public class BankSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MainMenu m;
+        m = new MainMenu();
+        m.pack();
+        m.pack();
+        m.setLocationRelativeTo(null);
+        m.show();
+                
+    }
+    
+}
